2ee36f42c0bc19c9c29ef1078e51beb26a83a4f0
Ephraim <aigbefoephraim@yahoo.com>
2022-10-08 14:02:27 +0100

    refactored database schema interface

--- src/database/types.ts
+++ src/database/types.ts
@@ -13,6 +13,22 @@ export interface UserWhereInterface {
   email?: string;
   phone_number?: string;
   token?: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+export interface UserInterface {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string | null | undefined;
+  phone_number: string;
+  token: string | null | undefined;
+  password: string | null | undefined;
+  is_verified: boolean;
+  is_blocked: boolean;
+  created_at: Date;
+  updated_at: Date;
 }
 
 export interface AccountCreateInterface {
@@ -32,25 +48,49 @@ export interface AccountWhereInterface {
   total_credit?: number;
   total_debit?: number;
   is_active?: boolean;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+export interface AccountInterface {
+  id: number;
+  user_id: number;
+  nuban: string;
+  available_balance: number;
+  book_balance: number;
+  status: "ACTIVE" | "INACTIVE" | "PND" | "PNC";
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface TransactionInterface {
+  id: number;
+  debit_account: string;
+  credit_account: string;
+  amount: string;
+  status: "PENDING" | "COMPLETED" | "FAILED";
+  comment: string | null | undefined;
+  ref: string;
+  created_at: Date;
+  updated_at: Date;
 }
 
 export interface TransactionCreateInterface {
-  to_account?: number;
-  from_account?: number;
+  debit_account: string;
+  credit_account: string;
   amount: string;
   status?: "PENDING" | "COMPLETED" | "FAILED";
-  type: "FUNDING" | "TRANSFER" | "WITHDRAWAL";
   comment?: string;
   ref: string;
 }
 
 export interface TransactionWhereInterface {
   id?: number;
-  to_account?: number;
-  from_account?: number;
+  debit_account?: string;
+  credit_account?: string;
   amount?: string;
   status?: "PENDING" | "COMPLETED" | "FAILED";
-  type?: "FUNDING" | "TRANSFER" | "WITHDRAWAL";
   ref?: string;
-  comment?: string;
+  created_at?: Date;
+  updated_at?: Date;
 }
